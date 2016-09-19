a63c7cf824fc987482dbe868d52cb980972439f0
Paoquime <paola.qmelendez@gmail.com>
2016-09-19 15:57:53 -0500

    agregando archivos iniciales

---
+++ js/main.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function() {
+	var boton = document.getElementById("enviar");
+	boton.addEventListener("click", function() {
+		var boxText = document.getElementById("box-text").value;
+		var texto = document.createElement("p");
+			texto.innerText = boxText;
+		var contenedor = document.getElementById("contenedor");
+			contenedor.childNodes[0];
+});
+  });
