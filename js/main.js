f61467447f8413c87b2eeedc4d159bcf402c897a
Paoquime <paola.qmelendez@gmail.com>
2016-09-20 21:05:08 -0500

    agregando cambio de color en contador y muestra en negativo

--- js/main.js
+++ js/main.js
@@ -1,27 +1,59 @@
 window.addEventListener("load", function() {
 	var boton = document.getElementById("go");
-	boton.disabled = true;
 	var boxText = document.getElementById("box-text");
+	var contador = document.getElementById("contador");
 
 	boton.addEventListener("click", function(e){
-
 		e.preventDefault();
+
 		var valor = boxText.value;
-			agregarText(valor);
-			boxText.value = "";
+		agregarText(valor);
+		boxText.value = "";
 		boton.disabled = true;
+		contador.innerText = "140";
+		contador.style.color = "black";
     });
+
+	boxText.addEventListener("keyup", function(){
+		cantidadCaracteres(boxText);
+	});
 
 	function agregarText(valor) {
 		var div = document.createElement("div");
 			div.innerText = valor;
 			div.classList.add("textConten");
+
 		var contenedor = document.getElementById("contenedor");
 			contenedor.insertBefore(div, contenedor.childNodes[0]);
 	}
 
-	boxText.addEventListener("keydown", function(){
-		boton.disabled = false;
-	}); 
+	function cantidadCaracteres(text){
+		var caracteres = text.value.length;
+			contador.innerText = 140 - caracteres;
+			
+		if (caracteres > 140 || caracteres == 0){
+			boton.disabled = true;
+		}else{
+			boton.disabled = false;
+		}
+
+		if (caracteres > 120){
+			contador.classList.add("yellow");
+		}else{
+			contador.classList.remove("yellow");
+		}
+
+		if (caracteres > 130){
+			contador.classList.add("red");
+		}else{
+			contador.classList.remove("red");
+		}
+
+		if (caracteres > 140){
+			contador.classList.add("orange");
+		}else{
+			contador.classList.remove("orange");
+		}
+	}
 });
 
